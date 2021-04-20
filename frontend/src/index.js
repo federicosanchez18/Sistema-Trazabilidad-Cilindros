ead4b1e33eea218be83d15a20c4c8dc09886d971
JonMaia <jonathannicolas.maia@gmail.com>
2021-04-20 18:29:45 -0300

    #3-Se implementa una tabla base con datos mockeado

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import AdminLayout from "layouts/Admin/Admin.js";
 import RTLLayout from "layouts/RTL/RTL.js";
+import CylindersList from "views/cylinders/list.js";
 
 import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
@@ -37,6 +38,7 @@ ReactDOM.render(
         <Switch>
           <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
           <Route path="/rtl" render={(props) => <RTLLayout {...props} />} />
+          <Route path="/cylinders" render={(props) => <CylindersList {...props} />} />
           <Redirect from="/" to="/admin/dashboard" />
         </Switch>
       </BrowserRouter>
