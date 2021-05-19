0f9a8bdf9b325adeb1a22567d786c136b99836df
Mauro Marino <macbook@MacBook-Pro.local>
2021-05-18 23:59:44 -0300

    #9 Agrego formularios de establecimientos

--- frontend/src/views/establecimientos/establecimiento.js
+++ frontend/src/views/establecimientos/establecimiento.js
@@ -18,16 +18,15 @@ function Establecimiento(props) {
           <div className="author">
             <img
               alt="..."
-              className="avatar"
               src={require("assets/img/opds.png").default}
             />
             <h5 className="title">Establecimiento</h5>
           </div>
           <p className="description">Razon Social: RONDA HORACIO ATILIO GILBERTO</p>
           <p className="description">Cuit: 20-14498244/5</p>
+          <p className="description">Nº Establecimiento: 876 </p>
+          <p className="description">Nº Presentación: SAN JUSTO</p>
           <p className="description">Planta: PLANTA SAN JUSTO</p>
-          <p className="description">Partido: LA MATANZA</p>
-          <p className="description">Localidad: SAN JUSTO</p>
           <p className="description">Domicilio: Calle: ARRIBEÑOS Nro: 3830 Ruta: ----------------------------- Km: ----------</p>
         </CardBody>
       </Card>
---
+++ frontend/src/views/establecimientos/formularioComercializador.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import FormularioBase from "views/establecimientos/formularioBase.js";
+import resolucion19896 from "assets/files/RESOLUCION_198_96.pdf";
+
+function FormularioComercializador(props) {
+    // Debería de obtenerse toda la info desde el backend
+    const [titulo] = useState("Establecimiento Comercializador")
+    const [reglamentaciones, setReglamentaciones] = useState([
+        {
+            "nombre": "Resolución 198/96",
+            "archivo": resolucion19896,
+        },
+        {
+            "nombre": "Resolución 738/07",
+            "archivo": "https://normas.gba.gob.ar/documentos/VJJ4kmcJ.html",
+        }
+    ])
+    const [pdfs, setPdfs] = useState([
+
+    ])
+
+    useEffect(() => {
+        // Acá se debería pegarle al backend para setear los valores que actualmente están mockeados
+    }, [])
+
+    return (
+        <>
+            <FormularioBase titulo={titulo} reglamentaciones={reglamentaciones} archivos={pdfs}/>
+        </>
+    );
+}
+
+export default FormularioComercializador;
