255918171e28c5072979e3d76ccaf5a73c9047f5
JonMaia <jonathannicolas.maia@gmail.com>
2021-05-24 15:32:05 -0300

    #12-Se agregan validaciones al crear un nuevo tramite contemplando la existencia de uno relacionado al tipo_tramite y establecimiento_id dado

--- backend/app/Http/Requests/Tramite/Store.php
+++ backend/app/Http/Requests/Tramite/Store.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests\Tramite;
 
 use Illuminate\Foundation\Http\FormRequest;
-use App\Rules\{RelacionadoConTipoTramite, DocumentosNecesariosSegunTipoTramite};
+use App\Rules\{RelacionadoConTipoTramite, DocumentosNecesariosSegunTipoTramite, ExisteTipoTramiteParaEstablecimiento};
 
 class Store extends FormRequest {
 
@@ -11,7 +11,7 @@ class Store extends FormRequest {
 
         return [
 			'establecimiento_id' => ['required', 'integer'],
-			'tipo_tramite_id' => ['required', 'integer', 'exists:tipo_tramites,id'],
+			'tipo_tramite_id' => ['required', 'integer', 'exists:tipo_tramites,id', new ExisteTipoTramiteParaEstablecimiento($this->establecimiento_id)],
 			'tipo_documento_ids' => ['required', 'array', new DocumentosNecesariosSegunTipoTramite($this->tipo_tramite_id)],
 			'tipo_documento_ids.*' => ['required', 'integer', 'exists:tipo_documentos,id', new RelacionadoConTipoTramite($this->tipo_tramite_id)],
         ];
