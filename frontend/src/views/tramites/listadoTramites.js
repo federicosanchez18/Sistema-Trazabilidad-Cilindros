eaad41d88e7d1779f68912dd97e06699e4b00f93
JonMaia <jonathannicolas.maia@gmail.com>
2021-06-05 12:41:28 -0300

    #17-Se corrigen identaciones, se quitan comentarios y se modifican temas relacionados al listado base de los tramites

--- frontend/src/views/tramites/listadoTramites.js
+++ frontend/src/views/tramites/listadoTramites.js
@@ -1,18 +1,17 @@
+import React, { useEffect } from "react";
+import ListadoBase from "./listadoBase.js";
 
-import React, { useEffect, useState } from "react";
-import ListadoBase from "./listadoBase.js"
+function ListadoTramites(props) {
+ 
+	useEffect(() => {
+	  // Acá se debería pegarle al backend para setear los valores que actualmente están mockeados
+	}, [])
 
-function ListadoTramites(props){
-  
-useEffect(() => {
-  // Acá se debería pegarle al backend para setear los valores que actualmente están mockeados
-}, [])
   return (
       <>
-          <ListadoBase titulo="Listado de Tramites"/> 
+        <ListadoBase/> 
       </>
   );
 }
-
 
 export default ListadoTramites;
